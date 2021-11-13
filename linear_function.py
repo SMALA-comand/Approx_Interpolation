d1e0aa74a3b9c8aed8bf3f6b9b0c8183f4fc863c
Mark Kozlov <37026894+Ubermensch124@users.noreply.github.com>
2021-11-13 14:59:39 +0300

    Косметические улучшения

--- linear_function.py
+++ linear_function.py
@@ -1,12 +1,13 @@
 import numpy as np
-import sympy
 from csv_reader import get_csv_coord
+
 
 def linear_function(coord):
     """
-
-    :param coord: координаты точек в форме массива [[x1, y1], [x2, y2], ...]
+        :param coord: координаты точек в форме массива [[x1, y1], [x2, y2], ...]
+        :return: возвращает кортеж (функция в виде строки, [[x1, y1, f1], ...], дисперсия)
     """
+
     ### для первого уравнения
     for_c1_1 = sum([i[0]**2 for i in coord])
     for_c0_1 = sum([i[0] for i in coord])
