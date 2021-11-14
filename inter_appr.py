06a518ab133f4030d64ba27b0f0f52030abf21cc
Archi20Rad <202103@edu.fa.ru>
2021-11-14 19:25:40 +0300

    Кубиком

--- inter_appr.py
+++ inter_appr.py
@@ -174,6 +174,83 @@ def newton_interpolation(coord, delta=0):
     else:
         return print('Равноотсояние точек не соблюденно, невозможно применить метод!')
 
+
+def cubic_spline_interpolation(coord, x0=[]):
+    """
+         Интерполяция методом Кубического сплайна
+        :param x0: Точки по x, для которых мы хотим посчитать интерполяцию  (список)
+        :param coord: Список с координатми
+    """
+    # Координаты изначальных точек
+    x_coord = [x[0] for x in coord]
+    y_coord = [y[1] for y in coord]
+
+    # Некоторые константы
+    sort_coord = sorted(x_coord)
+    l = len(y_coord)
+
+    # Словарь полинома, где ключ = xi, а значение = fi
+    l_res = dict()
+
+    x = np.asfarray(x_coord)
+    y = np.asfarray(y_coord)
+
+    # Сортируем данные
+    if np.any(np.diff(x) < 0):
+        indexes = np.argsort(x)
+        x = x[indexes]
+        y = y[indexes]
+
+    size = len(x)
+
+    xdiff = np.diff(x)
+    ydiff = np.diff(y)
+
+    # Работа с матрицами
+    Li = np.empty(size)
+    Li_1 = np.empty(size - 1)
+    z = np.empty(size)
+
+    # Заполняем диагонали Li и Li-1 и решаем матричное ур-ние L*Y = B
+    Li[0] = (2 * xdiff[0]) ** (0.5)
+    Li_1[0] = 0.0
+    B0 = 0.0
+    z[0] = B0 / Li[0]
+
+    for i in range(1, size - 1, 1):
+        Li_1[i] = xdiff[i - 1] / Li[i - 1]
+        Li[i] = (2 * (xdiff[i - 1] + xdiff[i]) ** (0.5) - Li_1[i - 1] * Li_1[i - 1])
+        Bi = 6 * (ydiff[i] / xdiff[i] - ydiff[i - 1] / xdiff[i - 1])
+        z[i] = (Bi - Li_1[i - 1] * z[i - 1]) / Li[i]
+
+    i = size - 1
+    Li_1[i - 1] = xdiff[-1] / Li[i - 1]
+    Li[i] = (2 * xdiff[-1] - Li_1[i - 1] * Li_1[i - 1]) ** (0.5)
+    Bi = 0.0
+    z[i] = (Bi - Li_1[i - 1] * z[i - 1]) / Li[i]
+
+    # Решаем L(T - транспонированное) * X = Y
+    i = size - 1
+    z[i] = z[i] / Li[i]
+    for i in range(size - 2, -1, -1):
+        z[i] = (z[i] - Li_1[i - 1] * z[i + 1]) / Li[i]
+
+    # Поиск индекса
+    index = x.searchsorted(x0)
+    np.clip(index, 1, size - 1, index)
+
+    xi1, xi0 = x[index], x[index - 1]
+    yi1, yi0 = y[index], y[index - 1]
+    zi1, zi0 = z[index], z[index - 1]
+    hi1 = xi1 - xi0
+
+    ans = zi0 / (6 * hi1) * (xi1 - x0) ** 3 + \
+          zi1 / (6 * hi1) * (x0 - xi0) ** 3 + \
+          (yi1 / hi1 - zi1 * hi1 / 6) * (x0 - xi0) + \
+          (yi0 / hi1 - zi0 * hi1 / 6) * (xi1 - x0)
+
+    return ans
+
 # Для теста:
 
 # check_distance(coord = [[0,-1],[1,-3],[2,3],[6,1187]])
